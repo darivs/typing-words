11a6979508cccf2b46e5339c03eb8a9aa87bc140
exceble <exceble@gmail.com>
2016-06-07 14:02:09 +0200

    added random word generator

--- IdeaProjects/TypingWords/src/TypingWords.java
+++ IdeaProjects/TypingWords/src/TypingWords.java
@@ -1,10 +1,35 @@
+import java.io.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Darius on 06.06.2016.
  */
-public class TypingWords {
 
-    public String spawnWord() {
-        return "spawned";
+public class TypingWords {
+    String[] words;
+
+    public String declareWord() {
+        String w = new String();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("words.txt"));
+            String row = br.readLine();
+
+            if (row != null) {
+                words = row.split(";");
+            }
+
+            int r = ThreadLocalRandom.current().nextInt(0, words.length - 1);
+            w =  words[r];
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return w;
     }
 
 
--- IdeaProjects/TypingWords/test/TypingWordsTest.java
+++ IdeaProjects/TypingWords/test/TypingWordsTest.java
@@ -1,5 +1,8 @@
+import org.junit.ComparisonFailure;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import java.io.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Darius on 06.06.2016.
@@ -8,13 +11,31 @@ import static org.junit.Assert.assertEquals;
 public class TypingWordsTest {
 
     @Test
-    public void spawn(){
-        String spwn = new TypingWords().spawnWord();
-        assertEquals("spawned", spwn);
+    public void getWord() throws FileNotFoundException {
+        String row = new String();
+        String rndWord = new TypingWords().declareWord();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("words.txt"));
+            row = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] expected = row.split(";");
+
+        for (int i = 0; i <= expected.length - 1; i++) {
+            try {
+                assertEquals(expected[i], rndWord);
+            } catch (ComparisonFailure e) { }
+        }
+
+        System.out.println(rndWord);
     }
 
     @Test
-    public void collision(){
+    public void collision()
+    {
         Boolean col = new TypingWords().collidedWord();
         assertEquals(true, col);
     }
