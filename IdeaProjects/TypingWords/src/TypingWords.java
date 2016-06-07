18a052bc0b7f387fd3799ef74e7278f668456589
Darius Tack <Darius Tack>
2016-06-07 09:18:50 +0200

    TypingWords

---
+++ IdeaProjects/TypingWords/src/TypingWords.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Darius on 06.06.2016.
+ */
+public class TypingWords {
+
+    public String spawnWord() {
+        return "spawned";
+    }
+
+
+    public String collidedWord() {
+        return "collided";
+    }
+
+}
---
+++ IdeaProjects/TypingWords/test/TypingWordsTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Darius on 06.06.2016.
+ */
+
+public class TypingWordsTest {
+
+    @Test
+    public void spawn(){
+        String spwn = new TypingWords().spawnWord();
+        assertEquals("spawned", spwn);
+    }
+
+    @Test
+    public void collision(){
+        String col = new TypingWords().collidedWord();
+        assertEquals("collided", col);
+    }
+}
